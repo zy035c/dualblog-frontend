ad33c8420dfb56fe7ce345b41c45ef6b07aaa36a
Ziyi Chen <zy035c@gmail.com>
2024-04-07 20:23:14 -0400

    fix token json

--- src/blog/user/login.jsx
+++ src/blog/user/login.jsx
@@ -13,7 +13,7 @@ const checkLoginStatus = async () => {
     return false;
   }
 
-  const result = await checkLogin({ token: token });
+  const result = await checkLogin({ "Token": token });
   return result.status === "success";
 };
 
